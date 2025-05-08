cda8be9283542472c9d8343a021b1a345db8e282
SowmithBalabhadra <sowmithbalabhadra@gmail.com>
2025-05-08 07:50:12 +0530

    Last try

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const port = 4000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174','http://localhost:5175'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174','http://localhost:5175','https://customershopspher.netlify.app/'],
   credentials: true,
 }));
 
